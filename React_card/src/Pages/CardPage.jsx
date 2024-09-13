ba99d60873522766f2af13a1858e1156bd6ffb7c
Yedida Guna Putra Nagendra Pavan <113666428+GunaPavan@users.noreply.github.com>
2024-09-13 23:12:08 +0530

    Add files via upload

---
+++ React_card/src/Pages/CardPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Card from '../components/Card'
+
+export default function CardPage() {
+  return (
+    <div className="w-full  bg-hero-pattern min-h-screen  flex flex-col justify-center items-center">
+       <h1 className="text-6xl font-sans text-white mb-10 font-extrabold">Study in Top European Countries</h1>
+        <Card/>
+    </div>
+  )
+}
---
+++ React_card/src/components/Header.jsx
@@ -0,0 +1,5 @@
+export default function Header() {
+  return (
+    <div className='w-full h-20 '>Header</div>
+  )
+}
